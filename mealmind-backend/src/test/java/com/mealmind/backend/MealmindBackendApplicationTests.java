1faaf3d0dc75681b36c8a87ea0bce774fee9ee04
Varun Saxena <varunsaxena314@gmail.com>
2025-12-22 19:44:30 -0500

    start

---
+++ mealmind-backend/src/test/java/com/mealmind/backend/MealmindBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealmind.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealmindBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
